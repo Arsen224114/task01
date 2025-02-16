66c6fb90e734f24a508f9c554ca5c4b38fd379f6
Arsen224114 <ayarmalitski@gmail.com>
2025-02-16 18:36:23 +0300

    Тестирование передачи данных в функцию по ссылке

--- Lesson34Solution/test03.cpp
+++ Lesson34Solution/test03.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int change(int* number) {
-	*number = *number * 10;
-}
-
-
-int main() {
-	int x = 100;
-
-	cout << "Before x = " << x << endl;
-	change(&x);
-	cout << "After x = " << x << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int change(int* number) {
+//	*number = *number * 10;
+//}
+//
+//
+//int main() {
+//	int x = 100;
+//
+//	cout << "Before x = " << x << endl;
+//	change(&x);
+//	cout << "After x = " << x << endl;
+//
+//	return 0;
+//}
