01230215e2d06465ff8af957f9dfe1de6145ca3a
Arsen224114 <ayarmalitski@gmail.com>
2025-02-16 19:38:38 +0300

    Сделали таск

---
+++ Task03/main.cpp
@@ -0,0 +1,16 @@
+#include "logic.h"
+using namespace std;
+
+int main() {
+	int number;
+	int pos;
+
+	cout << "Input your number and pos: ";
+	cin >> number >> pos;
+
+	round_change(number, pos);
+
+	cout << "Result number is " << number << endl;
+
+	return 0;
+}
