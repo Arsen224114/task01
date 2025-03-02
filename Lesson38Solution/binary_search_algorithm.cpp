9d59ca04b7558834768c86dbe96f7109f6dd0e24
Arsen224114 <ayarmalitski@gmail.com>
2025-03-02 18:12:47 +0300

    ...

--- Lesson38Solution/binary_search_algorithm.cpp
+++ Lesson38Solution/binary_search_algorithm.cpp
@@ -7,15 +7,18 @@ bool binary_search_algorithm(int* array, int size, int value) {
 	while (first <= last) {
 		int middle = (first + last) / 2;
 
-		if (value == array[middle]) 
+		if (value == array[middle]) {
 			return true;
+		}
+
+
+		else if (value < array[middle]) {
+			last = middle - 1;
+		}
+		else {
+			first = middle - 1;
+		}
 	}
-	else if (value < array[middle]) {
-	last = middle - 1;
-}
-	else {
-	first = middle - 1;
-}
 
 return false;
 }
